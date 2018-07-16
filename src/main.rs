d1432ec6a763b13cfe41b6de4540f20dc3ff6697
András Veres-Szentkirályi <vsza@vsza.hu>
2018-07-16 16:40:20 +0200

    working unixsstv/gen_values equivalent

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+fn main() {
+    use std::env;
+    use std::io;
+    use std::io::Read;
+    use std::io::Write;
+    use std::f64::consts::PI;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <samples per second>", args[0]);
+        return;
+    }
+
+    let samples_per_second: f64 = args[1].parse::<f64>().unwrap();
+
+    let spms = samples_per_second / 1000.0;
+    let mut offset = 0.0;
+    let factor = 2.0 * PI / samples_per_second;
+    let mut samples = 0.0;
+    let mut tx: i32;
+
+    let stdin = io::stdin();
+    let mut sil = stdin.lock();
+    let stdout = io::stdout();
+    let mut sol = stdout.lock();
+    let mut buffer = [0; 4];
+
+    loop {
+        match sil.read_exact(&mut buffer) {
+            Ok(_v) => {}
+            Err(_e) => { return; }
+        }
+        let freq: f32 = unsafe { std::mem::transmute(buffer) };
+        sil.read_exact(&mut buffer).unwrap();
+        let msec: f32 = unsafe { std::mem::transmute(buffer) };
+
+        samples += spms * msec as f64;
+        tx = samples as i32;
+        let freq_factor = freq as f64 * factor;
+        for sample in 0 .. tx {
+            let output: f32 = (sample as f64 * freq_factor + offset).sin() as f32;
+            buffer = unsafe { std::mem::transmute(output) };
+            sol.write_all(&buffer).unwrap();
+        }
+
+        offset += (tx + 1) as f64 * freq_factor;
+        samples -= tx as f64;
+    }
+}
